2aef48c0a635e57335b6674480cf32e64c837f9e
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-03 23:13:14 +0200

    add request data method in API Service

--- FlickerApp-Clone/FlickerApp-Clone/Networking/Shared /APIManger/APIService.swift
+++ FlickerApp-Clone/FlickerApp-Clone/Networking/Shared /APIManger/APIService.swift
@@ -7,34 +7,31 @@
 
 import Foundation
 
-//protocol APIServiceProtocol {
-//    func request<T: Decodable>(target: EndPointProtocol ,completion: @escaping (Result<T, Error>) -> Void)
-//}
 
 class APIService<T> where T:EndPointProtocol {
 
-//    func request<D: Decodable>(target: T ,completion: @escaping (Result<D, Error>) -> Void) {
-//        let request = target.request
-//    
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            do {
-//                if let error = error {
-//                    completion(.failure(error))
-//                    return
-//                }
-//                
-//                guard let data = data else {
-//                    preconditionFailure("No error was received but we also don't have data...")
-//                }
-//                
-//                let decodedObject = try JSONDecoder().decode(D.self, from: data)
-//                
-//                completion(.success(decodedObject))
-//            } catch {
-//                completion(.failure(error))
-//            }
-//        }.resume()
-//    }
+    func request<D: Decodable>(target: T ,completion: @escaping (Result<D, Error>) -> Void) {
+        let request = target.request
+    
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            do {
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                
+                guard let data = data else {
+                    preconditionFailure("No error was received but we also don't have data...")
+                }
+                
+                let decodedObject = try JSONDecoder().decode(D.self, from: data)
+                
+                completion(.success(decodedObject))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
 
 }
 
