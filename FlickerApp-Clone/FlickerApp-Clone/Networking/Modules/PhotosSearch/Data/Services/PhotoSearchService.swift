0ac1c096198643eeb64e17c8f12771d59af88814
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-04 01:33:40 +0200

    finish data layer for Photo search  network module
    in this layer , we added models , endpoint and service that utilize both of them

--- FlickerApp-Clone/FlickerApp-Clone/Networking/Modules/PhotosSearch/Data/Services/PhotoSearchService.swift
+++ FlickerApp-Clone/FlickerApp-Clone/Networking/Modules/PhotosSearch/Data/Services/PhotoSearchService.swift
@@ -11,8 +11,8 @@ import Foundation
 
 class PhotoSearchService: APIService<PhotoSearchEndPoint> {
   
-//    func serachPhotoByText(_ text: String, completion: @escaping (Result<Feed, Error>) -> Void){
-//      //  request(target: .searhPhoto(searchText: text), completion: completion)
-//    }
+    func serachPhotoByText(_ text: String, completion: @escaping (Result<PhotoSearchResult, Error>) -> Void){
+        request(target: .searhPhoto(searchText: text), completion: completion)
+    }
  
 }
