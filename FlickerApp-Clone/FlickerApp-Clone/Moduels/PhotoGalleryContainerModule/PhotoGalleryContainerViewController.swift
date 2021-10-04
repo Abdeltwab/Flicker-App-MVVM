ef681e7e0500e5f333ef1ffce107a89b4e4ef435
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-04 12:32:36 +0200

    add basic structure for business (UI) layer for the app
     - added three modules for photo search  , Gallery and Container module
     - remove main storyboard and replace it with xib files

---
+++ FlickerApp-Clone/FlickerApp-Clone/Moduels/PhotoGalleryContainerModule/PhotoGalleryContainerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryContainerViewController.swift
+//  FlickerApp-Clone
+//
+//  Created by Abdeltawab Mohamed on 04/10/2021.
+//
+
+import UIKit
+
+class PhotoGalleryContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
