1f5147d82919cf33bc17b9d951f5d65b133a01db
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-05 12:28:23 +0200

    add search bar to PhotoGalleryContainerViewController , and setup it's UI and Delegate

--- FlickerApp-Clone/FlickerApp-Clone/Moduels/PhotoGalleryContainerModule/PhotoGalleryContainerViewController.swift
+++ FlickerApp-Clone/FlickerApp-Clone/Moduels/PhotoGalleryContainerModule/PhotoGalleryContainerViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class PhotoGalleryContainerViewController: UIViewController {
+class PhotoGalleryContainerViewController: UIViewController, UISearchBarDelegate {
+    
+
+    
+    var searchController: UISearchController!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
     }
 
+}
+
+//MARK: - UI
+extension PhotoGalleryContainerViewController{
+    
+    private func setupUI(){
+        setupNavigationBarTitle()
+        setupSearchController()
+    }
+
+    private func setupSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.view.backgroundColor = .white
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search PlacHolder"
+        searchController.hidesNavigationBarDuringPresentation = true
+        navigationItem.searchController = searchController
+    }
+
+    private func setupNavigationBarTitle(){
+        self.title = "Image Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
 }
