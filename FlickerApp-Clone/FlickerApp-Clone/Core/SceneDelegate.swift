ee352d3fcf3827e7a03a6c1a8b8328a9562bb3e1
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-05 12:28:01 +0200

    embedded PhotoGalleryContainerViewController into navigation  bar  and set it as a project start point

--- FlickerApp-Clone/FlickerApp-Clone/Core/SceneDelegate.swift
+++ FlickerApp-Clone/FlickerApp-Clone/Core/SceneDelegate.swift
@@ -13,13 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-     
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
-               let window = UIWindow(windowScene: windowScene)
-               window.rootViewController = PhotoGalleryContainerViewController()
-               self.window = window
-               window.makeKeyAndVisible()
+        
+        let window = UIWindow(windowScene: windowScene)
+        let nav = UINavigationController(rootViewController: PhotoGalleryContainerViewController())
+        window.rootViewController = nav
+        self.window = window
+        window.makeKeyAndVisible()
         
     }
 
