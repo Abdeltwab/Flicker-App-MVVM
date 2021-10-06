483cf1d55ebb90da964030f677a0c6f01ccbfc7c
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-06 19:15:01 +0200

    add photoDetails viewContoler outlets

--- FlickerApp-Clone/FlickerApp-Clone/Moduels/PhotoDetailsModule/View/PhotoDetailsViewController.swift
+++ FlickerApp-Clone/FlickerApp-Clone/Moduels/PhotoDetailsModule/View/PhotoDetailsViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class PhotoDetailsViewController: UIViewController ,PhotoDetailsViewControllerProtocol {
     
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var getPhotoSizesBtn: UIButton!
+    
     var viewModel: PhotoDetailsViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+}
+
+extension PhotoDetailsViewController {
+    
 }
