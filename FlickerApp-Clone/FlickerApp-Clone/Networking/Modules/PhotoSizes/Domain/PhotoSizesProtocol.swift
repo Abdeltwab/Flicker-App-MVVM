aaf9bfaa387b10bfeb10af1522a3d09af3aba543
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-06 16:25:57 +0200

    add getPhotoSize by id use case

--- FlickerApp-Clone/FlickerApp-Clone/Networking/Modules/PhotoSizes/Domain/PhotoSizesProtocol.swift
+++ FlickerApp-Clone/FlickerApp-Clone/Networking/Modules/PhotoSizes/Domain/PhotoSizesProtocol.swift
@@ -9,5 +9,31 @@ import RxSwift
 import RxCocoa
 
 protocol PhotoSizesProtocol {
+
+    //MARK: - Methods
+
+    func getPhotoSizes(id:String) -> Observable<(PhotoSizesResponse?,Error?)>
+
+    //MARK: - Services
     
+    var service: PhotoSizesService {get}
+
+}
+
+extension PhotoSizesProtocol {
+    
+    func getPhotoSizes(id:String) -> Observable<(PhotoSizesResponse?,Error?)> {
+        
+        return Observable.create { observer in
+            service.getPhotoSizeby(id:id) { result in
+                switch result {
+                case let .success(sizesResponse):
+                    observer.onNext((sizesResponse,nil))
+                case let .failure(error):
+                    observer.onNext((nil,error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
 }
