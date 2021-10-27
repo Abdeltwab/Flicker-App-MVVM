b54563477eb6f27f999502f35e0b9b7004e2eb09
Abdeltwab <Abdeltwab.elhussin@gmail.com>
2021-10-27 21:00:42 +0200

    add basic setup for testing Photo Galley Module

---
+++ FlickerApp-Clone/FlickerApp-Clone/Moduels/PhotoGalleryModule/Testing/PhotoGalleryTest/PhotoGalleryViewModelTest.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoGalleryViewModelTest.swift
+//  FlickerApp-Clone
+//
+//  Created by Abdeltawab Mohamed on 27/10/2021.
+//
+
+import Foundation
+/*
+  - This should have method that rececive "Cat"
+  - use the mock Use case
+  - Assert the resutl is equal
+ 
+ */
